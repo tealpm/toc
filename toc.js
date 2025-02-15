cbf436291c0d83eac3b47e9f427bd25332312348
Paula Maregal <contact@paulamaregal.com>
2025-02-15 13:14:39 +0100

    Extract several functions

--- toc.js
+++ toc.js
@@ -2,75 +2,97 @@
 /*
 Requirements in the HTML: 
 - an empty element like a <div> with the id="toc"
-- the HTML heading elements must load before this function
-*/ 
+- the HTML heading elements must load before this code, so generateTOC() has been added to a DOMContentLoaded event
+*/
 
-(function(){
-    if (document.getElementById("toc")){
-        const HEADINGS = document.querySelectorAll("h2, h3, h4, h5, h6");
-        if (HEADINGS.length > 0){
-            let getLevels = function(nodeList){ 
-                let itemSet = new Set;
-                for (const item of nodeList)
-                    itemSet.add(item.tagName);
-                let endings = [...itemSet].map(item => parseFloat(item.slice(-1)));
-                return endings.sort().reverse(); // From highest (inner) to lowest (outer)
+let TOC;
+let HEADINGS;
+
+document.addEventListener("DOMContentLoaded", generateTOC);
+
+function generateTOC() {
+    TOC = document.getElementById("toc");
+    HEADINGS = document.querySelectorAll("h2, h3, h4, h5, h6");
+    if (TOC != null && HEADINGS.length > 0) {
+        addTitle("Content");
+        createList();
+    }
+};
+
+function addTitle(text) {
+    const TITLE = document.createElement("p");
+    TITLE.innerText = text;
+    TOC.append(TITLE);
+}
+
+function createList() {
+    const levels = getLevelsInDescendingOrder();
+    const parentLevels = getParentLevels(levels);
+    const topLevel = parseInt(levels.slice(-1));
+    let tag, row, list, currentLevel, previousLevel, parentList;
+    let sublistNumber = 0;
+    for (const [index, heading] of HEADINGS.entries()) {
+        tag = heading.tagName;
+        previousLevel = (index !== 0) ? currentLevel : topLevel - 1;
+        currentLevel = parseInt(tag.slice(-1));
+        row = document.createElement("li");
+        setRow(row, index, heading);
+        if (currentLevel === previousLevel && currentLevel !== topLevel) {
+            document.getElementById(`sublist${sublistNumber}-${tag}`).appendChild(row);
         }
-            const TOC = document.getElementById("toc");
-            let addTitle = function(text){
-                const TITLE = document.createElement("p");
-                TITLE.innerText = text;
-                TOC.appendChild(TITLE);
-            }
-            const LEVELS = getLevels(HEADINGS); 
-            const BASE_LEVEL = parseInt(LEVELS.slice(-1));
-            const BASE_HEADING = "H" + BASE_LEVEL;
-            let previousItem = parseInt(LEVELS.slice(-1))-1;
-            let sublistNumber = 0;
-            let setRow = function(row, index, heading){
-                row.innerText = `${heading.innerText}`;
-                heading.setAttribute("data-title", `t${index}`); // So it's compatible with HEADINGS having an ID already
-                row.setAttribute("data-heading", `t${index}`);
-                row.setAttribute("onclick", "document.querySelector(`[data-title='${this.dataset.heading}']`).scrollIntoView()");
-            } 
-            let createList = function(nodeList){
-                for (const [index, heading] of nodeList.entries()){
-                    let row = document.createElement("li");
-                    setRow(row, index, heading);
-                    let currentItem = parseInt(heading.tagName.slice(-1));
-                    if (heading.tagName !== BASE_HEADING && index > 0)
-                        previousItem = parseInt(HEADINGS[index-1].tagName.slice(-1));
-                    if (currentItem === previousItem && currentItem !== BASE_LEVEL) // Sibling row
-                        document.getElementById(`sublist${sublistNumber}-${heading.tagName}`).appendChild(row);
-                    else {
-                        if (heading.tagName === BASE_HEADING)
-                            sublistNumber++;
-                        if (document.getElementById(`sublist${sublistNumber}-${heading.tagName}`)) 
-                            document.getElementById(`sublist${sublistNumber}-${heading.tagName}`).removeAttribute("id");
-                        // To ensure child always appends to the closest parent 
-                        let newList = document.createElement("ul");
-                        newList.setAttribute("id", `sublist${sublistNumber}-${heading.tagName}`);
-                        newList.appendChild(row);
-                        let itemAdded = false;
-                        for (let i of LEVELS.slice(LEVELS.indexOf(parseFloat(currentItem))+1)){
-                            if (document.getElementById(`sublist${sublistNumber}-H${i}`)){
-                                let parentList = document.getElementById(`sublist${sublistNumber}-H${i}`);
-                                if (currentItem > previousItem)
-                                    parentList.appendChild(newList); 
-                                // If levels are skipped, ul-indent is NOT bigger (ex: if H4 after H2, it will look like an H3)
-                                else if (currentItem < previousItem)
-                                    parentList.append(newList);
-                                itemAdded = true;
-                                break;
-                            }
-                        }
-                        if (!itemAdded)
-                            TOC.appendChild(newList);
-                    }
-                }
-            }
-            addTitle("Content");
-            createList(HEADINGS);
+        else {
+            sublistNumber = (tag === `H${topLevel}`) ? sublistNumber + 1 : sublistNumber;
+            list = createSublist(tag, index, sublistNumber, row);
+            parentList = getClosestParent(parentLevels, currentLevel, sublistNumber);
+            parentList.lastElementChild.appendChild(list);
         }
     }
-}());
+}
+
+function getLevelsInDescendingOrder() {
+    const itemSet = new Set;
+    for (const item of HEADINGS)
+        itemSet.add(item.tagName);
+    const endings = [...itemSet].map(item => parseFloat(item.slice(-1)));
+    return endings.sort().reverse();
+}
+
+function getParentLevels(descendingLevels) {
+    const parentLevels = [];
+    let currentLevel;
+    for (let i = 0; i < descendingLevels.length; i++) {
+        currentLevel = descendingLevels[i];
+        parentLevels[currentLevel] = descendingLevels.slice(descendingLevels.indexOf(parseFloat(currentLevel)) + 1);
+    }
+    return parentLevels;
+}
+
+function setRow(row, index, heading) {
+    row.innerText = `${heading.innerText}`;
+    heading.setAttribute("data-title", `t${index}`); // So it's compatible with HEADINGS already having an ID
+    row.setAttribute("data-heading", `t${index}`);
+    row.setAttribute("onclick", "document.querySelector(`[data-title='${this.dataset.heading}']`).scrollIntoView()");
+}
+
+function createSublist(tag, position, number, row) {
+    const identifier = `sublist${number}-${tag}`;
+    // To ensure child always appends to the closest parent | cb = "changed by" a certain heading
+    if (document.getElementById(identifier))
+        document.getElementById(identifier).id = `${identifier}-cb${position}`;
+    const newList = document.createElement("ul");
+    newList.setAttribute("id", identifier);
+    newList.appendChild(row);
+    return newList;
+}
+
+function getClosestParent(parentLevels, level, listNumber) {
+    if (parentLevels.length > 0) {
+        for (const i of parentLevels[level]) {
+            if (document.getElementById(`sublist${listNumber}-H${i}`)) {
+                return document.getElementById(`sublist${listNumber}-H${i}`);
+            }
+        }
+    }
+    return TOC;
+    // If levels are skipped, ul-indent is NOT bigger (ex: if H4 after H2, it will look like an H3)
+}
